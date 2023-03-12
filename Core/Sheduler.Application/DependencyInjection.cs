9d2737212ce267b67c811d2734dcdcf465f9edb0
jejikeh <jejikeh@gmail.com>
2023-03-12 22:46:54 +0300

    work winth validation itnterop

--- Core/Sheduler.Application/DependencyInjection.cs
+++ Core/Sheduler.Application/DependencyInjection.cs
@@ -1,6 +1,18 @@
-﻿namespace Sheduler.Application;
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Sheduler.Application.Common.Behaviors;
 
-public class DependencyInjection
+namespace Sheduler.Application;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        serviceCollection.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        serviceCollection.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        return serviceCollection;
+    }
 }
