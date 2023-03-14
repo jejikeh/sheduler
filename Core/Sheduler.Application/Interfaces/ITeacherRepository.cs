251590d90456f7aab873f863b03c9722e03277ee
jejikeh <jejikeh@gmail.com>
2023-03-14 18:17:44 +0300

    remove for now lesson db context

--- Core/Sheduler.Application/Interfaces/ITeacherRepository.cs
+++ Core/Sheduler.Application/Interfaces/ITeacherRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Sheduler.Application.Interfaces;
+﻿using Sheduler.Domain.Models;
 
-public class ITeacherRepository
+namespace Sheduler.Application.Interfaces;
+
+public interface ITeacherRepository
 {
-    
+    public Task<ICollection<Teacher>> GetAllTeachers(int userId);
+    public Task<Teacher> GetTeacherByName(string name, int userId);
+    public Task<Teacher> CreateTeacher(int userId, string name);
+    public Task<Teacher> UpdateTeacher(string teacherName, int userId, string newName);
+    public Task DeleteTeacher(string name, int userId);
+
 }
--- Core/Sheduler.Application/Interfaces/ITeachersDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sheduler.Application.Common.Interfaces;
-using Sheduler.Domain.Models;
-
-namespace Sheduler.Application.Interfaces;
-
-public interface ITeachersDbContext : IDbContext<Teacher>
-{
-    public Task<Teacher?> FindTeacherAsync(string name);
-}
