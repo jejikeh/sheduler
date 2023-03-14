9ce6e2bc83dc6f488bac9389bd9b15107ada0969
jejikeh <jejikeh@gmail.com>
2023-03-14 18:20:07 +0300

    remove identity

--- Presentation/Sheduler.Identity/Sheduler.Identity/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Sheduler.Identity.Models;
-
-public class AppUser : IdentityUser
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
